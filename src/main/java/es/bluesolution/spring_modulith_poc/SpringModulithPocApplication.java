deb4a51011a56dd64160c290f30f9c4d4ad29933
Javier Godon <javier.godon@gmail.com>
2024-01-13 16:33:50 +0100

    configurations

---
+++ src/main/java/es/bluesolution/spring_modulith_poc/SpringModulithPocApplication.java
@@ -0,0 +1,14 @@
+package es.bluesolution.spring_modulith_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringModulithPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModulithPocApplication.class, args);
+	}
+
+}
